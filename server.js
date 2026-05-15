91de5b5ab6f6a5945d320f66f4aa4128f2e2cfa5
danyalqazi <danyalqazi1@yahoo.com>
2026-05-15 11:41:02 +0500

    Add fs import to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
+const fs = require("fs");
 
 dotenv.config();
 require("./database");
